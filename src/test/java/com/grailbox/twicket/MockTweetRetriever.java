69fed4f299fb277baade6fc999f2c23d2431f563
Rob Warner <rwarner@grailbox.com>
2010-05-17 16:57:33 -0400

    Add some tests

---
+++ src/test/java/com/grailbox/twicket/MockTweetRetriever.java
@@ -0,0 +1,13 @@
+package com.grailbox.twicket;
+
+import java.util.List;
+
+import winterwell.jtwitter.Twitter.Status;
+
+public class MockTweetRetriever implements TweetRetriever {
+
+  public List<Status> retrieveTweets() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
--- src/test/java/com/grailbox/twicket/TestPublicTimelinePage.java
+++ src/test/java/com/grailbox/twicket/TestPublicTimelinePage.java
@@ -1,18 +1,12 @@
 package com.grailbox.twicket;
 
-import com.grailbox.twicket.HomePage;
-import com.grailbox.twicket.WicketApplication;
-
 import junit.framework.TestCase;
 
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.util.tester.WicketTester;
 
-/**
- * Simple test using the WicketTester
- */
-public class TestHomePage extends TestCase {
+public class TestPublicTimelinePage extends TestCase {
   private WicketTester tester;
 
   @Override
@@ -21,11 +15,10 @@ public class TestHomePage extends TestCase {
   }
 
   public void testRenderMyPage() {
-    tester.startPage(HomePage.class);
-    tester.assertRenderedPage(HomePage.class);
-    tester.assertComponent("publicTimeline", BookmarkablePageLink.class);
+    tester.startPage(PublicTimelinePage.class);
+    tester.assertRenderedPage(PublicTimelinePage.class);
+    tester.assertComponent("home", BookmarkablePageLink.class);
     tester.assertComponent("feedback", FeedbackPanel.class);
-    tester.assertComponent("searchPanel", SearchPanel.class);
     tester.assertComponent("tweetListPanel", TweetListPanel.class);
   }
 }
